57b52dc7eeb549b5235307334fcbcf69b5776e3f
Marie <marie.sacksick@posteo.net>
2025-05-19 16:42:55 +0200

    change dataset

---
+++ python_files/demo.py
@@ -0,0 +1,82 @@
+# %%
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import FunctionTransformer, OrdinalEncoder
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.datasets import fetch_openml
+
+from skrub import tabular_learner, TableReport
+
+import skore
+from skore import EstimatorReport
+from skore_hub_project.project.project import Project
+
+# %% [markdown]
+# Fetch the dataset. We will use the credit fraud dataset from skrub.
+
+# %%
+X, y = fetch_openml("adult", version=2, as_frame=True, return_X_y=True)
+# %%
+TableReport(X)
+# %%
+y.value_counts()
+
+# %% 
+y = 1*(y == ">50K")
+
+# %%
+X_train, X_test, y_train, y_test = skore.train_test_split(X, y, random_state=1)
+
+# %% [markdown]
+# Simpler is better.
+# Let's do a simple baseline.
+
+# %%
+baseline = tabular_learner('classification')
+baseline
+# %%
+baseline_report = EstimatorReport(baseline, X_train = X_train, y_train=y_train, X_test = X_test, y_test = y_test)
+baseline_report.help()
+
+# %% 
+baseline_report.metrics.report_metrics()
+
+# %%
+# create project
+project = Project(name="project demo - census", tenant="Probabl")
+# %%
+project.put("baseline", baseline_report)
+
+# %% [markdown]
+# Let's go a bit further in that baseline by optimizing the parameters.
+
+# %%
+from sklearn.model_selection import GridSearchCV
+
+baseline_2 = GridSearchCV(
+                estimator = baseline,
+                param_grid = {
+                    "histgradientboostingclassifier__learning_rate":[0.01, 0.1, 0.2],
+                    "histgradientboostingclassifier__max_depth":[1, 3, 5],
+                    "histgradientboostingclassifier__max_leaf_nodes":[30, 60, 90],
+                },
+                cv = 5,
+                n_jobs = -1,
+                refit=True,
+                scoring="neg_log_loss",
+            )
+baseline_2
+
+# %%
+baseline_report_2 = EstimatorReport(baseline_2, X_train = X_train, y_train=y_train, X_test = X_test, y_test = y_test)
+# %%
+project.put("baseline_2", baseline_report_2)
+# %%
+baseline_report_2.metrics.report_metrics()
+# %%
+comp = skore.ComparisonReport([baseline_report, baseline_report_2])
+comp.help()
+# %%
+comp.metrics.report_metrics(pos_label = 1)
+
+# %% [markdown]
+# DEMO PART 2 - after superior review
