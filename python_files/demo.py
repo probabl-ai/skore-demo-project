9967a56a988131cc1c48b8ec982fc70d7541ab7c
Marie <marie.sacksick@posteo.net>
2025-05-20 09:49:16 +0200

    reformat

--- python_files/demo.py
+++ python_files/demo.py
@@ -2,25 +2,29 @@
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import FunctionTransformer, OrdinalEncoder
 from sklearn.compose import ColumnTransformer, make_column_selector
+
 # %% [markdown]
-# Fetch the dataset. We will use the census dataset from openml.  
-# It's a binary classification problem, where the target is whether a person earns more than 50K a year.  
-# https://www.openml.org/search?type=data&sort=runs&id=1590&status=active  
+# Fetch the dataset. We will use the census dataset from openml.
+# It's a binary classification problem, where the target is whether a person earns more than 50K a year.
+# https://www.openml.org/search?type=data&sort=runs&id=1590&status=active
 
 # %%
 from sklearn.datasets import fetch_openml
+
 X, y = fetch_openml("adult", version=2, as_frame=True, return_X_y=True)
 # %%
 from skrub import TableReport
+
 TableReport(X)
 # %%
 y.value_counts()
 
-# %% 
-y = 1*(y == ">50K")
+# %%
+y = 1 * (y == ">50K")
 
 # %%
 import skore
+
 X_train, X_test, y_train, y_test = skore.train_test_split(X, y, random_state=1)
 
 # %% [markdown]
@@ -29,10 +33,12 @@ X_train, X_test, y_train, y_test = skore.train_test_split(X, y, random_state=1)
 
 # %%
 from skrub import tabular_learner
-baseline = tabular_learner('classification')
+
+baseline = tabular_learner("classification")
 baseline
 # %%
 from skore import EstimatorReport
+
 baseline_report = EstimatorReport(
     baseline,
     X_train=X_train,
@@ -42,15 +48,18 @@ baseline_report = EstimatorReport(
 )
 baseline_report.help()
 
-# %% 
+# %%
 baseline_report.metrics.report_metrics()
 
 # %%
 # create or connect to project
 from skore_hub_project.project.project import Project
+
 project = Project(name="project demo - census", tenant="Probabl")
 # %%
-baseline_report = EstimatorReport(baseline, X_train = X_train, y_train=y_train, X_test = X_test, y_test = y_test)
+baseline_report = EstimatorReport(
+    baseline, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test
+)
 project.put("baseline", baseline_report)
 
 # %% [markdown]
@@ -60,21 +69,23 @@ project.put("baseline", baseline_report)
 from sklearn.model_selection import GridSearchCV
 
 tuned_baseline = GridSearchCV(
-                estimator = baseline,
-                param_grid = {
-                    "histgradientboostingclassifier__learning_rate":[0.01, 0.1, 0.2],
-                    "histgradientboostingclassifier__max_depth":[1, 3, 5],
-                    "histgradientboostingclassifier__max_leaf_nodes":[30, 60, 90],
-                },
-                cv = 5,
-                n_jobs = -1,
-                refit=True,
-                scoring="neg_log_loss",
-            )
+    estimator=baseline,
+    param_grid={
+        "histgradientboostingclassifier__learning_rate": [0.01, 0.1, 0.2],
+        "histgradientboostingclassifier__max_depth": [1, 3, 5],
+        "histgradientboostingclassifier__max_leaf_nodes": [30, 60, 90],
+    },
+    cv=5,
+    n_jobs=-1,
+    refit=True,
+    scoring="neg_log_loss",
+)
 tuned_baseline
 
 # %%
-tuned_baseline_report = EstimatorReport(tuned_baseline, X_train = X_train, y_train=y_train, X_test = X_test, y_test = y_test)
+tuned_baseline_report = EstimatorReport(
+    tuned_baseline, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test
+)
 # %%
 project.put("tuned_baseline", tuned_baseline_report)
 # %%
@@ -83,7 +94,7 @@ tuned_baseline_report.metrics.report_metrics()
 comp = skore.ComparisonReport([baseline_report, tuned_baseline])
 comp.help()
 # %%
-comp.metrics.report_metrics(pos_label = 1)
+comp.metrics.report_metrics(pos_label=1)
 
 # %% [markdown]
 # DEMO PART 2 - after superior review
